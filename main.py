bd598194ed8ad2d9ffb9e53c3b1ce1855415962a
tcaldwell368 <83304821+tcaldwell368@users.noreply.github.com>
2021-05-29 00:08:46 +0100

    Upload
    pop more columns, filled NaN values with 'None' , removed rows that had particular columns as NaN, added API request

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import pandas as pd
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 30)
+pd.set_option('display.width', 1000)
 df = pd.read_csv('tripadvisor_european_restaurants_reduced.zip', compression='zip')
 print(df.head())
 hotel = pd.DataFrame(df)
 print(hotel.columns.tolist())
 hotel.pop('keywords')
+hotel.pop('city')
+hotel.pop('province')
+hotel.pop('address')
+hotel.pop('atmosphere')
 print(hotel.shape)
-hotel_refine = hotel.dropna(how = 'all' , subset =['value','price_level'] )
+hotel['awards']= hotel['awards'].fillna('None')
+hotel['features']= hotel['features'].fillna('None')
+hotel_refine = hotel[pd.notnull(hotel['value'])]
 print(hotel_refine.shape)
-print(hotel_refine.head())
+hotel_refine1 = hotel_refine[pd.notnull(hotel_refine['price_level'])]
+print(hotel_refine1.shape)
+hotel_refine2 = hotel_refine1[pd.notnull(hotel_refine1['avg_rating'])]
+print(hotel_refine2.shape)
+hotel_refine3 = hotel_refine2[pd.notnull(hotel_refine2['cuisines'])]
+print(hotel_refine3.shape)
+hotel_refine4 = hotel_refine3[pd.notnull(hotel_refine3['region'])]
+print(hotel_refine4.shape)
+print(hotel_refine4.head())
+print(hotel_refine4.isnull().sum())
+print(hotel_refine4['country'].value_counts())
 
 
+#import requests
+
+#url = "https://travel-advisor.p.rapidapi.com/restaurants/list"
+
+#querystring = {"location_id":"293919","restaurant_tagcategory":"10591","restaurant_tagcategory_standalone":"10591","currency":"USD","lunit":"km","limit":"30","open_now":"false","lang":"en_US"}
+
+#headers = {
+ #   'x-rapidapi-key': "3e5ee6b200msh8bafd806e6e5a41p142121jsn0f80884ee934",
+  #  'x-rapidapi-host': "travel-advisor.p.rapidapi.com"
+   # }
+
+#response = requests.request("GET", url, headers=headers, params=querystring)
+
+# print(response.text)
